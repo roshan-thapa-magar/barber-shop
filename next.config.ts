851721c517d5f94f378978bdf24b712855c86c17
Roshan Thapa Magar <roshan@octspace.com>
2025-09-18 08:24:46 +0545

    google image

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
